8b89ea0ec5722bfdda4af93e846a9771a4c5d3c4
fmp <fpagliughi@mindspring.com>
2018-10-28 22:11:07 -0400

    Added a device detect example

---
+++ examples/iio_detect.rs
@@ -0,0 +1,43 @@
+// industrial-io/examples/iio_detect.rs
+//
+// Simple Rust IIO example. This lists the devices found in the default context.
+//
+
+extern crate industrial_io as iio;
+use std::process;
+
+fn main() {
+    let ctx = iio::Context::new().unwrap_or_else(|_err| {
+        println!("Couldn't open default IIO context");
+        process::exit(1);
+    });
+
+    let mut trigs = Vec::new();
+
+    if ctx.num_devices() == 0 {
+        println!("No devices in the default IIO context");
+    }
+    else {
+        println!("IIO Devices:");
+        for dev in ctx.devices() {
+            if dev.is_trigger() {
+                if let Some(id) = dev.id() {
+                    trigs.push(id);
+                }
+            }
+            else {
+                print!("  {} ", dev.id().unwrap_or_default());
+                print!("[{}]", dev.name().unwrap_or_default());
+                println!(": {} channel(s)", dev.num_channels());
+            }
+        }
+
+        if !trigs.is_empty() {
+            println!("\nTriggers Devices:");
+            for s in trigs {
+                println!("  {}", s);
+            }
+        }
+    }
+}
+
--- examples/iio_readraw.rs
+++ examples/iio_readraw.rs
@@ -9,17 +9,29 @@ use schedule_recv::periodic;
 fn main() -> iio::Result<()> {
     let ctx = iio::Context::new().expect("Couldn't open IIO Context");
     let dev = ctx.get_device(0).unwrap();
+    let unknown = "unknown".to_string();
 
     let tick = periodic(Duration::from_millis(1000));
+
+    for chan in dev.channels() {
+        if chan.id() != Some("timestamp".to_string()) {
+            print!("{} ", chan.id().unwrap_or(unknown.clone()));
+        }
+    }
+    println!();
 
     loop {
         tick.recv().unwrap();
         for chan in dev.channels() {
-            if let Ok(val) = chan.attr_read_int("raw") {
-                print!("  {}", val);
+            if chan.id() == Some("timestamp".to_string()) {
             }
             else {
-                print!("  xxxx");
+                if let Ok(val) = chan.attr_read_int("raw") {
+                    print!(" {:6}", val);
+                }
+                else {
+                    print!(" xxxxxx");
+                }
             }
         }
         println!("");
